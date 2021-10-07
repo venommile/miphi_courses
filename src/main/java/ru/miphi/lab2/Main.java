178f732f2ff359f4ce620c8786119e47650a62cd
Mishail <54024036+venommile@users.noreply.github.com>
2021-10-07 19:32:33 +0300

    done lab 2

---
+++ src/main/java/ru/miphi/lab2/Main.java
@@ -0,0 +1,67 @@
+package ru.miphi.lab2;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input_string;
+        input_string = sc.nextLine();
+        String str = "";
+//        task1(input_string);
+        task2(input_string);
+
+
+    }
+
+    public static void task1(String input_string) {
+
+        HashMap<String, Integer> map = new HashMap();
+        String str = "";
+        for (int i = 0; i < input_string.length(); i++) {
+            if (input_string.charAt(i) == ' ') {
+                if (str != "") {
+                    if (map.containsKey(str)) {
+                        int k = map.get(str);
+                        k++;
+                        map.put(str, k++);
+                    } else {
+                        map.put(str, 1);
+                    }
+                }
+                str = "";
+            } else {
+                str += input_string.charAt(i);
+            }
+        }
+        for (String key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key));
+        }
+
+    }
+
+    public static void task2(String input_string) {
+        HashSet<String> set = new HashSet();
+        String str = "";
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < input_string.length(); i++) {
+            if (input_string.charAt(i) == ' ') {
+                if (str != "") {
+                    if (!set.contains(str)) {
+                        set.add(str);
+                        list.add(str);
+                    }
+
+                }
+                str = "";
+            } else {
+                str += input_string.charAt(i);
+            }
+        }
+        for (String key : list) {
+            System.out.print(key+" ");
+        }
+
+
+    }
+}
